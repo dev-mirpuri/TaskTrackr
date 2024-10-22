f886790480275f7202f82ae353fdbe31fcf56597
dev-mirpuri <149519587+dev-mirpuri@users.noreply.github.com>
2024-10-22 15:15:56 +0100

    vertically center all tab buttons

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -5,9 +5,18 @@ import { useGlobalState } from "@/app/context/globalProvider"
 import Image from 'next/image';
 import menu from "@/app/utils/menu";
 import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 
 function Sidebar () {
     const {theme} = useGlobalState();
+
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const handleClick = (link:string) => {
+        router.push(link)
+    };
+
     return (
         <SidebarStyled theme={theme}>
             <div className="profile">
@@ -22,14 +31,19 @@ function Sidebar () {
             </div>
             <ul className="nav-items">
                 {menu.map((item) => {
-                    return <li>
+                    const link = item.link;
+                    return <li className={`nav-item ${pathname===link ? "active" : ""}`} 
+                        onClick={() => {
+                            handleClick(link);
+                        }}>
                         {item.icon}
-                        <Link href={item.link}>
+                        <Link href={link}>
                             {item.title}
                         </Link>
                     </li>;
                 })}
             </ul>
+            <button></button>
         </SidebarStyled>
     )
 }
@@ -40,6 +54,10 @@ const SidebarStyled = styled.nav`
     background-color: ${(props) => props.theme.colorBg2};
     border: 2px solid ${(props) => props.theme.borderColor2};
     border-radius: 1rem;
+
+    display: flex;
+        flex-direction: column;
+        justify-content: space-between;
 `;
 
 export default Sidebar
