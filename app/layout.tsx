3d59037a77c3bbfa1bd5fde0b0c0ba34aea58ece
dev-mirpuri <149519587+dev-mirpuri@users.noreply.github.com>
2024-10-22 11:10:05 +0100

    added globalstyleprovider for application

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Sidebar from "./components/sidebar/sidebar";
 import "./globals.css";
+import GlobalStyleProvider from "./providers/GlobalStyleProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GlobalStyleProvider>
         <Sidebar />
         {children}
+        </GlobalStyleProvider>
       </body>
     </html>
   );
---
+++ app/providers/GlobalStyleProvider.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+interface Props {
+    children: React.ReactNode;
+}
+
+function GlobalStyleProvider({children}: Props) {
+    return <>{children}</>;
+}
+
+export default GlobalStyleProvider
