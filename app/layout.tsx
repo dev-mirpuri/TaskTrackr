4e3ba0cff9d21355ce4a1796be42bb72ffbd2f2f
dev-mirpuri <149519587+dev-mirpuri@users.noreply.github.com>
2024-10-22 11:03:56 +0100

    initial sidebar testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Sidebar from "./components/sidebar/sidebar";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Sidebar />
         {children}
       </body>
     </html>
